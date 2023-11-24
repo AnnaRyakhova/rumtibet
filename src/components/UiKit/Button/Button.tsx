a161ffc96621d9ebcced325224aea51a554b980a
Ryakhova Anna <anna-muzychuk@mail.ru>
2023-11-24 14:29:18 +0300

    Create UiKit for Button

--- src/components/UiKit/Button/Button.tsx
+++ src/components/UiKit/Button/Button.tsx
@@ -3,15 +3,17 @@ import styles from './button.module.css';
 import cl from 'classnames';
 
 type Size = 'normal' | 'big'
+type Color = 'dark' | 'light'
 
 interface Button {
     size?: Size;
+    color?: Color;
     children: string;
     className?: string;
 }
 
-export const Button: FC<Button> = ({ size = 'big', children, className }) => {
+export const Button: FC<Button> = ({ size = 'big', color = 'dark', children, className }) => {
     return (
-        <button className={cl(styles.button, styles[size], className)}>{children}</button>
+        <button className={cl(styles.button, styles[size], styles[color], className)}>{children}</button>
     )
 }
