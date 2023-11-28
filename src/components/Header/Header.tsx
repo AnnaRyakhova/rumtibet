1ca872bae3679084df1a5b90955cf2c7ab7ac15c
AnnaRyakhova <138567659+AnnaRyakhova@users.noreply.github.com>
2023-11-28 10:23:28 +0300

    Create Header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,48 @@
+import styles from './Header.module.css'
+import { Button } from '../UiKit/Button/Button'
+import { BurgerIcon, CloseIcon, LogoIcon, MenuArrowIcon } from '../UiKit/Icons/Icons'
+import { useState } from 'react'
+import cn from 'classnames'
+
+
+export const Header = () => {
+    const [isOpen, setIsOpen] = useState(false)
+
+    return (
+        <div className={styles.root}>
+            <div className='container'>
+                <div className={styles.wrapper}>
+                    <LogoIcon className={styles.logo} />
+
+                    <nav className={styles.nav}>
+                        <ul className={cn(styles.navItems, { [styles.open]: isOpen })}>
+                            <li className={styles.lii}>
+                                <MenuArrowIcon className={styles.menuArrow} /><a href="#">Главная</a>
+                            </li>
+                            <li>
+                                <MenuArrowIcon className={styles.menuArrow} /><a href="#">Про гида</a>
+                            </li>
+                            <li>
+                                <MenuArrowIcon className={styles.menuArrow} /><a href="#">Программа тура</a>
+                            </li>
+                            <li>
+                                <MenuArrowIcon className={styles.menuArrow} /><a href="#">Стоимость</a>
+                            </li>
+                            <li>
+                                <MenuArrowIcon className={styles.menuArrow} /><a href="#">Блог</a>
+                            </li>
+                            <li>
+                                <MenuArrowIcon className={styles.menuArrow} /><a href="#">Контакты</a>
+                            </li>
+                        </ul>
+
+                        <Button size='normal' className={styles.button}>Консультация</Button>
+                    </nav>
+                    <button onClick={() => setIsOpen(!isOpen)} className={styles.mobileMenuIcon}>
+                        {isOpen ? <CloseIcon className={styles.closeIcon} /> : <BurgerIcon />}
+                    </button>
+                </div>
+            </div>
+        </div>
+    )
+}
