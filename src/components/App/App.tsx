6edf778877e3e51e390260491566351eea488ec5
Ryakhova Anna <anna-muzychuk@mail.ru>
2023-12-01 13:24:57 +0300

    Create About and change code style

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,6 +3,7 @@ import { Offer } from '../Offer/Offer';
 import { About } from '../About/About';
 import { FirstScreen } from '../FirstScreen/FirstScreen';
 import { Header } from '../Header/Header';
+import { About } from '../About/About';
 
 export const App = () => (
   <>
