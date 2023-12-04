97b49b5d1d50bf732501344f13b25d9989c42ad6
AnnaRyakhova <138567659+AnnaRyakhova@users.noreply.github.com>
2023-12-04 10:56:42 +0300

    Create FirstScreen (#6)
    Create FirstScreen

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,13 @@
 import './App.css';
 import { Offer } from '../Offer/Offer';
 import { About } from '../About/About';
+import { FirstScreen } from '../FirstScreen/FirstScreen';
+import { Header } from '../Header/Header';
 
 export const App = () => (
   <>
+    <Header />
+    <FirstScreen />
     <About />
     <Offer />
   </>
