8cacdb4c741b25c966cef0c806eaa63ba3581dcd
Ryakhova Anna <anna-muzychuk@mail.ru>
2023-12-04 15:22:36 +0300

    Remove imports

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,7 +3,6 @@ import { Offer } from '../Offer/Offer';
 import { About } from '../About/About';
 import { FirstScreen } from '../FirstScreen/FirstScreen';
 import { Header } from '../Header/Header';
-import { About } from '../About/About';
 
 export const App = () => (
   <>
