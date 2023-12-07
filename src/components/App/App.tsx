2b4347b2dd32711165b11ca84f9e175c316ec441
Ryakhova Anna <anna-muzychuk@mail.ru>
2023-12-07 16:10:11 +0300

    Create Directions

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,11 +2,13 @@ import './App.css';
 import { Offer } from '../Offer/Offer';
 import { FirstScreen } from '../FirstScreen/FirstScreen';
 import { Header } from '../Header/Header';
+import { Directions } from '../Directions/Directions';
 
 export const App = () => (
   <>
     <Header />
     <FirstScreen />
     <Offer />
+    <Directions />
   </>
 )
