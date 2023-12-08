75361f169ac7230da6cd1940ae1f96d392edfe4f
Ryakhova Anna <anna-muzychuk@mail.ru>
2023-12-08 15:00:14 +0300

    Create Icon and change code style

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,9 @@
 import styles from './Header.module.css'
 import { Button } from '../UiKit/Button/Button'
-import { BurgerIcon, CloseIcon, LogoIcon, MenuArrowIcon } from '../UiKit/Icons/Icons'
+
 import { useState } from 'react'
 import cn from 'classnames'
-
+import { Icon } from '../Icon/Icon'
 
 export const Header = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -11,34 +11,42 @@ export const Header = () => {
     return (
         <div className={styles.root}>
             <div className={cn('container', styles.wrapper)}>
-                <LogoIcon className={styles.logo} />
+                <Icon variant='logo' color='light' className={styles.logo} />
 
                 <nav className={styles.nav}>
                     <ul className={cn(styles.navItems, { [styles.open]: isOpen })}>
                         <li className={styles.lii}>
-                            <MenuArrowIcon className={styles.menuArrow} /><a href="#">Главная</a>
+                            <Icon variant='menuArrow' color='light' className={styles.menuArrow} />
+                            <a href="#">Главная</a>
                         </li>
                         <li>
-                            <MenuArrowIcon className={styles.menuArrow} /><a href="#">Про гида</a>
+                            <Icon variant='menuArrow' color='light' className={styles.menuArrow} />
+                            <a href="#">Про гида</a>
                         </li>
                         <li>
-                            <MenuArrowIcon className={styles.menuArrow} /><a href="#">Программа тура</a>
+                            <Icon variant='menuArrow' color='light' className={styles.menuArrow} />
+                            <a href="#">Программа тура</a>
                         </li>
                         <li>
-                            <MenuArrowIcon className={styles.menuArrow} /><a href="#">Стоимость</a>
+                            <Icon variant='menuArrow' color='light' className={styles.menuArrow} />
+                            <a href="#">Стоимость</a>
                         </li>
                         <li>
-                            <MenuArrowIcon className={styles.menuArrow} /><a href="#">Блог</a>
+                            <Icon variant='menuArrow' color='light' className={styles.menuArrow} />
+                            <a href="#">Блог</a>
                         </li>
                         <li>
-                            <MenuArrowIcon className={styles.menuArrow} /><a href="#">Контакты</a>
+                            <Icon variant='menuArrow' color='light' className={styles.menuArrow} />
+                            <a href="#">Контакты</a>
                         </li>
                     </ul>
 
                     <Button size='normal' className={styles.button}>Консультация</Button>
                 </nav>
                 <button onClick={() => setIsOpen(!isOpen)} className={styles.mobileMenuIcon}>
-                    {isOpen ? <CloseIcon className={styles.closeIcon} /> : <BurgerIcon />}
+                    {isOpen
+                        ? <Icon variant='closeMenu' color='white' className={styles.closeIcon} />
+                        : <Icon variant='burger' color='white' />}
                 </button>
             </div>
         </div>
