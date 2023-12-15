d3112a8373806d93d021c681a82adbd38c9101eb
Ryakhova Anna <anna-muzychuk@mail.ru>
2023-12-15 21:17:13 +0300

    Add Fancybox library

--- src/components/Photos/Photos.tsx
+++ src/components/Photos/Photos.tsx
@@ -1,34 +1,73 @@
+import { Icon } from '../Icon/Icon'
 import { Button } from '../UiKit/Button/Button'
 import { Typography } from '../UiKit/Typography/Typography'
 import styles from './Photos.module.css'
 import cn from 'classnames'
+import Fancybox from './Fancybox'
 
 export const Photos = () => {
-    return (
-        <div className={cn(styles.wrapper, 'container')}>
-            <div className={styles.title}>
-                <Typography variant="accentSubtitle" color="accentColor">
-                    фото-отчет
-                </Typography>
-                <Typography variant="h2">Делимся впечатлениями</Typography>
-            </div>
+  return (
+    <div className={cn(styles.wrapper, 'container')}>
+      <div className={styles.title}>
+        <Typography variant="accentSubtitle" color="accentColor">
+          фото-отчет
+        </Typography>
+        <Typography variant="h2">Делимся впечатлениями</Typography>
+      </div>
 
-            <div className={styles.photos}>
-                <div
-                    className={cn(styles.photo, styles.wideItem)}
-                    style={{ backgroundImage: "url('/photos/photo_1.jpeg')" }}
-                />
-                <div className={styles.photo} style={{ backgroundImage: "url('/photos/photo_2.jpeg')" }} />
-                <div className={styles.photo} style={{ backgroundImage: "url('/photos/photo_3.jpeg')" }} />
+      <Fancybox className={styles.photos}>
+        <a
+          data-fancybox="gallery"
+          href="/photos/photo_1.jpeg"
+          className={cn(styles.photo, styles.wideItem)}
+          style={{ backgroundImage: "url('/photos/photo_1.jpeg')" }}
+        >
+          <Icon variant="loupe" color="white" className={styles.loupe} />
+        </a>
+        <a
+          data-fancybox="gallery"
+          href="/photos/photo_2.jpeg"
+          className={styles.photo}
+          style={{ backgroundImage: "url('/photos/photo_2.jpeg')" }}
+        >
+          <Icon variant="loupe" color="white" className={styles.loupe} />{' '}
+        </a>
+        <a
+          data-fancybox="gallery"
+          href="/photos/photo_3.jpeg"
+          className={styles.photo}
+          style={{ backgroundImage: "url('/photos/photo_3.jpeg')" }}
+        >
+          <Icon variant="loupe" color="white" className={styles.loupe} />{' '}
+        </a>
 
-                <div className={styles.photo} style={{ backgroundImage: "url('/photos/photo_4.jpeg')" }} />
-                <div
-                    className={cn(styles.photo, styles.wideItem)}
-                    style={{ backgroundImage: "url('/photos/photo_5.jpeg')" }}
-                />
-                <div className={styles.photo} style={{ backgroundImage: "url('/photos/photo_6.jpeg')" }} />
-            </div>
-            <Button>Наш pinterest</Button>
-        </div>
-    )
+        <a
+          data-fancybox="gallery"
+          href="/photos/photo_4.jpeg"
+          className={styles.photo}
+          style={{ backgroundImage: "url('/photos/photo_4.jpeg')" }}
+        >
+          <Icon variant="loupe" color="white" className={styles.loupe} />{' '}
+        </a>
+        <a
+          data-fancybox="gallery"
+          href="/photos/photo_5.jpeg"
+          className={cn(styles.photo, styles.wideItem)}
+          style={{ backgroundImage: "url('/photos/photo_5.jpeg')" }}
+        >
+          <Icon variant="loupe" color="white" className={styles.loupe} />
+        </a>
+        <a
+          data-fancybox="gallery"
+          href="/photos/photo_6.jpeg"
+          className={styles.photo}
+          style={{ backgroundImage: "url('/photos/photo_6.jpeg')" }}
+        >
+          <Icon variant="loupe" color="white" className={styles.loupe} />
+        </a>
+      </Fancybox>
+
+      <Button>Наш pinterest</Button>
+    </div>
+  )
 }
