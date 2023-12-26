415c00b7d8d82c1e36e44eacc54134356a5becb5
AnnaRyakhova <138567659+AnnaRyakhova@users.noreply.github.com>
2023-12-26 11:38:22 +0300

    Add react-datepicker (#16)

--- src/components/FirstScreen/FirstScreen.tsx
+++ src/components/FirstScreen/FirstScreen.tsx
@@ -2,10 +2,63 @@ import styles from './FirstScreen.module.css'
 import { Typography } from '../UiKit/Typography/Typography'
 import { Button } from '../UiKit/Button/Button'
 import { Select } from './Select/Select'
-import selects from './constants'
 import cn from 'classnames'
+import DatePicker from 'react-datepicker'
+import { useState } from 'react'
+import 'react-datepicker/dist/react-datepicker.css'
+import { Icon } from '../Icon/Icon'
+
+const locations = [
+  {
+    value: 'selectTitle',
+    option: 'Локация для тура',
+  },
+  {
+    value: 'Beijing',
+    option: 'Пекин',
+  },
+  {
+    value: 'Lhasa',
+    option: 'Лхаса',
+  },
+  {
+    value: 'Shanghai',
+    option: 'Шанхай',
+  },
+]
+
+const members = [
+  {
+    value: 'selectTitle',
+    option: 'Участники',
+  },
+  {
+    value: 'Beijing',
+    option: 'Пекин',
+  },
+  {
+    value: 'Lhasa',
+    option: 'Лхаса',
+  },
+  {
+    value: 'Shanghai',
+    option: 'Шанхай',
+  },
+]
+
+type Range = [Date, Date]
 
 export const FirstScreen = () => {
+  const [startDate, setStartDate] = useState<Date>()
+  const [endDate, setEndDate] = useState<Date>()
+
+  const handleChange = (range: Range): void => {
+    console.log(range)
+    const [startDate, endDate] = range
+    setStartDate(startDate)
+    setEndDate(endDate)
+  }
+
   return (
     <div className={styles.root}>
       <div className={cn('container', styles.wrapper)}>
@@ -15,9 +68,41 @@ export const FirstScreen = () => {
           </Typography>
           <div className={styles.formWrapper}>
             <form className={styles.form}>
-              {selects.map(({ label, labelFor, options }, index) => (
-                <Select label={label} labelFor={labelFor} options={options} key={index} />
-              ))}
+              <Select
+                label="выберите из списка"
+                labelFor="location"
+                options={locations}
+                className={styles.input}
+                labelClass={styles.label}
+              />
+
+              <div className={styles.datePicker}>
+                <DatePicker
+                  selected={startDate}
+                  onChange={handleChange}
+                  startDate={startDate}
+                  endDate={endDate}
+                  selectsRange
+                  className={styles.input}
+                  placeholderText="Дата похода"
+                  wrapperClassName={styles.datePickerWrapper}
+                  dateFormat="dd/MM/yyyy"
+                  closeOnScroll
+                />
+                <Icon variant="calendar" color="white" className={styles.calendarIcon} />
+                <Typography variant="subtitle" color="lightColor" className={styles.label}>
+                  укажите диапазон
+                </Typography>
+              </div>
+
+              <Select
+                label="минимум 4 человека"
+                labelFor="members"
+                options={members}
+                className={styles.input}
+                labelClass={styles.label}
+              />
+
               <Button color="light" className={styles.button}>
                 Найти программу
               </Button>
--- src/components/FirstScreen/Select/Select.tsx
+++ src/components/FirstScreen/Select/Select.tsx
@@ -10,17 +10,19 @@ interface Select {
   label: string
   labelFor: string
   options: Options[]
+  className?: string
+  labelClass?: string
 }
 
-export const Select: FC<Select> = ({ label, labelFor, options }) => {
+export const Select: FC<Select> = ({ label, labelFor, options, className, labelClass }) => {
   return (
-    <div className={styles.selectWrapper}>
-      <label htmlFor={labelFor} className={styles.label}>
+    <div className={className}>
+      <label htmlFor={labelFor} className={labelClass}>
         <Typography variant="subtitle" color="lightColor">
           {label}
         </Typography>
       </label>
-      <select id={labelFor}>
+      <select id={labelFor} className={styles.select}>
         {options.map(({ value, option }, index) => (
           <option value={value} key={index}>
             {option}
