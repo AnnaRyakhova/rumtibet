70916f34f2797f88e04029c1af9b00fd38e4a17e
AnnaRyakhova <138567659+AnnaRyakhova@users.noreply.github.com>
2024-01-30 16:18:19 +0500

    Add modal (#24)
    * Add modal
    
    * Change code style and delete ModalForm as component

--- src/components/FirstScreen/FirstScreen.tsx
+++ src/components/FirstScreen/FirstScreen.tsx
@@ -1,70 +1,13 @@
 import styles from './FirstScreen.module.css'
 import { Typography } from '../UiKit/Typography/Typography'
 import { Button } from '../UiKit/Button/Button'
-import { Select } from './Select/Select'
 import cn from 'classnames'
-import DatePicker from 'react-datepicker'
-import { useState } from 'react'
 import 'react-datepicker/dist/react-datepicker.css'
-import { Icon } from '../Icon/Icon'
-import { Form, INITIAL_FORM, LOCATIONS } from './constants'
-import { getOptions, getLabel, isRangeContainsMembers, getDefaultMembersCount, getLocation } from './utils'
-import { useValidation } from '../../hooks'
-import { toast } from 'sonner'
-import moment from 'moment'
-
-export type Range = [Date, Date]
+import { Form } from './Form/Form'
+import { useState } from 'react'
 
 export const FirstScreen = () => {
-  const [form, setForm] = useState<Form>(INITIAL_FORM)
-
-  const handleRange = (range: Range): void => {
-    const [startDate, endDate] = range
-    setForm((oldForm) => ({ ...oldForm, startDate: startDate, endDate: endDate }))
-  }
-
-  const handleLocation = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const location = e.target.value
-
-    if (isRangeContainsMembers(form.members, location)) {
-      setForm((oldForm) => ({ ...oldForm, location: location }))
-    } else {
-      setForm((oldForm) => ({ ...oldForm, location: location, members: '' }))
-    }
-
-    const defaultMembers = getDefaultMembersCount(location)
-
-    if (defaultMembers) {
-      setForm((oldForm) => ({ ...oldForm, location: location, members: defaultMembers }))
-    }
-  }
-
-  const handleMembers = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    setForm((oldForm) => ({ ...oldForm, members: e.target.value }))
-  }
-
-  const RULE_IS_EMPTY = { isEmpty: true }
-  const { location, endDate, members } = form
-
-  const locationValid = useValidation(location, RULE_IS_EMPTY)
-  const dateValid = useValidation(endDate, RULE_IS_EMPTY)
-  const membersValid = useValidation(members, RULE_IS_EMPTY)
-
-  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
-    e.preventDefault()
-
-    const invalid = locationValid.isEmpty || dateValid.isEmpty || membersValid.isEmpty
-
-    if (invalid) {
-      toast.error('Пожалуйста, заполните все поля')
-    } else {
-      const startDate = moment(form.startDate).format('DD.MM.YYYY')
-      const endDate = moment(form.endDate).format('DD.MM.YYYY')
-      const location = getLocation(form.location)
-
-      toast.success(`Выполняем поиск тура в ${location} с ${startDate} по ${endDate}`)
-    }
-  }
+  const [modalActive, setModalActive] = useState(false)
 
   return (
     <div className={styles.root}>
@@ -74,54 +17,18 @@ export const FirstScreen = () => {
             Насладись прогулкой в горах с командой единомышленников
           </Typography>
 
-          <form className={styles.form} onSubmit={handleSubmit} noValidate>
-            <Select
-              value={form.location}
-              label="выберите из списка"
-              labelFor="location"
-              options={LOCATIONS}
-              className={styles.input}
-              labelClass={styles.label}
-              onChange={handleLocation}
-            />
+          <Form type="form" />
 
-            <div className={styles.datePicker}>
-              <DatePicker
-                selected={form.startDate}
-                onChange={handleRange}
-                startDate={form.startDate}
-                endDate={form.endDate}
-                selectsRange
-                className={styles.input}
-                placeholderText="Дата похода"
-                wrapperClassName={styles.datePickerWrapper}
-                dateFormat="dd.MM.yyyy"
-                closeOnScroll
-              />
-              <Icon variant="calendar" color="white" className={styles.calendarIcon} />
-              <Typography variant="subtitle" color="lightColor" className={styles.label}>
-                укажите диапазон
-              </Typography>
-            </div>
-
-            <Select
-              value={form.members}
-              label={getLabel(form.location)}
-              onChange={handleMembers}
-              labelFor="members"
-              options={getOptions(form.location)}
-              className={styles.input}
-              labelClass={styles.label}
-            />
-
-            <Button color="light" className={styles.button}>
-              Найти программу
-            </Button>
-          </form>
-
-          <Button color="light" className={styles.mobButton}>
+          <Button color="light" className={styles.mobButton} onClick={() => setModalActive(true)}>
             Найти программу
           </Button>
+
+          <div
+            className={cn(styles.modal, modalActive ? [styles.active] : [styles.notActive])}
+            onClick={() => setModalActive(false)}
+          >
+            <Form type="modalForm" />
+          </div>
         </div>
       </div>
     </div>
---
+++ src/components/FirstScreen/Form/Form.tsx
@@ -0,0 +1,113 @@
+import styles from './Form.module.css'
+import { useState } from 'react'
+import { FormState, INITIAL_FORM, LOCATIONS } from '../constants'
+import { getDefaultMembersCount, getLabel, getLocation, getOptions, isRangeContainsMembers } from '../utils'
+import moment from 'moment'
+import { toast } from 'sonner'
+import { useValidation } from '../../../hooks'
+import { Select } from '../Select/Select'
+import DatePicker from 'react-datepicker'
+import { Icon } from '../../Icon/Icon'
+import { Typography } from '../../UiKit/Typography/Typography'
+import { Button } from '../../UiKit/Button/Button'
+
+type Range = [Date, Date]
+
+export const Form = ({ type }: { type: string }) => {
+  const [form, setForm] = useState<FormState>(INITIAL_FORM)
+
+  const handleRange = (range: Range): void => {
+    const [startDate, endDate] = range
+    setForm((oldForm) => ({ ...oldForm, startDate: startDate, endDate: endDate }))
+  }
+
+  const handleLocation = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const location = e.target.value
+
+    if (isRangeContainsMembers(form.members, location)) {
+      setForm((oldForm) => ({ ...oldForm, location: location }))
+    } else {
+      setForm((oldForm) => ({ ...oldForm, location: location, members: '' }))
+    }
+
+    const defaultMembers = getDefaultMembersCount(location)
+
+    if (defaultMembers) {
+      setForm((oldForm) => ({ ...oldForm, location: location, members: defaultMembers }))
+    }
+  }
+
+  const handleMembers = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    setForm((oldForm) => ({ ...oldForm, members: e.target.value }))
+  }
+
+  const RULE_IS_EMPTY = { isEmpty: true }
+  const { location, endDate, members } = form
+
+  const locationValid = useValidation(location, RULE_IS_EMPTY)
+  const dateValid = useValidation(endDate, RULE_IS_EMPTY)
+  const membersValid = useValidation(members, RULE_IS_EMPTY)
+
+  const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    const invalid = locationValid.isEmpty || dateValid.isEmpty || membersValid.isEmpty
+
+    if (invalid) {
+      toast.error('Пожалуйста, заполните все поля')
+    } else {
+      const startDate = moment(form.startDate).format('DD.MM.YYYY')
+      const endDate = moment(form.endDate).format('DD.MM.YYYY')
+      const location = getLocation(form.location)
+
+      toast.success(`Выполняем поиск тура в ${location} с ${startDate} по ${endDate}`)
+    }
+  }
+
+  return (
+    <form className={styles[type]} onSubmit={handleSubmit} noValidate onClick={(e) => e.stopPropagation()}>
+      <Select
+        value={form.location}
+        label="выберите из списка"
+        labelFor="location"
+        options={LOCATIONS}
+        className={styles.input}
+        labelClass={styles.label}
+        onChange={handleLocation}
+      />
+
+      <div className={styles.datePicker}>
+        <DatePicker
+          selected={form.startDate}
+          onChange={handleRange}
+          startDate={form.startDate}
+          endDate={form.endDate}
+          selectsRange
+          className={styles.input}
+          placeholderText="Дата похода"
+          wrapperClassName={styles.datePickerWrapper}
+          dateFormat="dd.MM.yyyy"
+          closeOnScroll
+        />
+        <Icon variant="calendar" color="white" className={styles.calendarIcon} />
+        <Typography variant="subtitle" color="lightColor" className={styles.label}>
+          укажите диапазон
+        </Typography>
+      </div>
+
+      <Select
+        value={form.members}
+        label={getLabel(form.location)}
+        onChange={handleMembers}
+        labelFor="members"
+        options={getOptions(form.location)}
+        className={styles.input}
+        labelClass={styles.label}
+      />
+
+      <Button color="light" className={styles.button}>
+        Найти программу
+      </Button>
+    </form>
+  )
+}
